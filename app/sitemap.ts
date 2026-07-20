00d284b08e084b318f49ba7876de3ee72d1cc45c
Zain Bharde <105802338+Zzzain24@users.noreply.github.com>
2026-07-20 10:06:50 -0700

    Code cleanup

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -9,22 +9,34 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 1,
     },
     {
-      url: "https://damilareoo.xyz/#work",
-      lastModified: new Date(),
-      changeFrequency: "weekly",
-      priority: 0.8,
-    },
-    {
       url: "https://damilareoo.xyz/#about",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.7,
     },
     {
-      url: "https://damilareoo.xyz/#across-the-web",
+      url: "https://damilareoo.xyz/#experience",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.7,
+    },
+    {
+      url: "https://damilareoo.xyz/#projects",
+      lastModified: new Date(),
+      changeFrequency: "weekly",
+      priority: 0.8,
+    },
+    {
+      url: "https://damilareoo.xyz/#contact",
       lastModified: new Date(),
       changeFrequency: "monthly",
       priority: 0.5,
+    },
+    {
+      url: "https://damilareoo.xyz/photography",
+      lastModified: new Date(),
+      changeFrequency: "monthly",
+      priority: 0.6,
     },
   ]
 }
